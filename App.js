f6fb8235d2f39a1b988b7c6db14adb7105af0fcb
Wojciech Stanisz <wojciech.stanisz@swmansion.com>
2020-05-22 23:19:00 +0200

    little refactor

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect, useRef} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import Expo, { Notifications } from "expo";
 import * as Permissions from 'expo-permissions';
 import Scene from './Scene'
 import { AsyncStorage, Dimensions, Image } from 'react-native';
 
-const screen = { 
-  height: Math.round(Dimensions.get('window').height), 
+const screen = {
+  height: Math.round(Dimensions.get('window').height),
   width: Math.round(Dimensions.get('window').width)
 };
 
@@ -14,107 +14,114 @@ export default function App() {
   let [hunger, setHunger] = useState(0);
   let [lastFood, setlastFood] = useState(Date.now());
   let [isDead, setIsDead] = useState(false);
-
   let [delay, setDelay] = useState(1000);
 
   useEffect(() => {
     setTimeout(async () => {
-    try {
-      const lastFoodSaved = await AsyncStorage.getItem('@lastFoodTimestamp');
-      if (lastFoodSaved) {
-        setlastFood(lastFoodSaved);
-        hunger = Math.floor((Date.now() - lastFoodSaved)/1000);
-        if (hunger > 100) {
-          setHunger(100);
-          setIsDead(true);
-        } else {
-          setHunger(hunger);
+      try {
+        const lastFoodSaved = await AsyncStorage.getItem('@lastFoodTimestamp');
+        if (lastFoodSaved) {
+          setlastFood(lastFoodSaved);
+          hunger = Math.floor((Date.now() - lastFoodSaved) / 1000);
+          checkHunged(hunger);
         }
+      } catch (e) {
+
       }
-    } catch (e) {
-  
-    } 
     }, 0);
-    return ;
+    return;
   }, []);
 
   useInterval(() => {
-    hunger = Math.floor((Date.now() - lastFood)/1000);
+    hunger = Math.floor((Date.now() - lastFood) / 1000);
+    checkHunged(hunger);
+
+  }, delay);
+
+  const checkHunged = (hunger) => {
     if (hunger > 100) {
       setHunger(100);
       setIsDead(true);
     } else {
       setHunger(hunger);
     }
-    
-  }, delay);
+  }
 
   const sendNotification = async (msToNotification) => {
-      Notifications.cancelAllScheduledNotificationsAsync();
-      const { status } = await Permissions.askAsync(
-        Permissions.NOTIFICATIONS,
+    Notifications.cancelAllScheduledNotificationsAsync();
+    const { status } = await Permissions.askAsync(
+      Permissions.NOTIFICATIONS,
+    );
+    if (status === 'granted') {
+      const lol = await Notifications.scheduleLocalNotificationAsync(
+        { title: "Animal", body: "Your pet is hungry. 😢" },
+        { time: new Date().getTime() + msToNotification }
       );
-      if (status === 'granted') {
-        const lol = await Notifications.scheduleLocalNotificationAsync(
-          { title: "Animal", body: "Your pet is hungry. 😢" },
-          { time: new Date().getTime() + msToNotification }
-        );
-      }
+    }
+  }
+
+  const respawn = () => {
+    sendNotification(80000);
+    setIsDead(false);
+    setlastFood(Date.now());
+    setHunger(0);
+  }
+
+  const giveFood = async () => {
+    sendNotification(80000);
+    setlastFood(Date.now());
+    try {
+      const nowDateString = String(Date.now());
+      await AsyncStorage.setItem('@lastFoodTimestamp', nowDateString);
+    } catch (e) {
+    }
+    setHunger(0);
   }
 
   return (
     <View>
-      <View style={{height: screen.height, width: screen.width, position: 'absolute'}}>
+      <View style={{ height: screen.height, width: screen.width, position: 'absolute' }}>
         <Scene />
       </View>
-      <View style={{height: 40, paddingTop: 120}}>
-      <TouchableOpacity
-        style={{height: 100}}
-        onPress={async () => {
-
-        }}
-      >
-        <Text style={{height: 20}}>Hunger: {100 - hunger}</Text>
-        <Text style={{height: 20}}>Send Notification</Text>
-        <Text style={{height: 20}}>Open menu</Text>
-      </TouchableOpacity>
-      <TouchableOpacity 
-        style={{height: 100}}
-        onPress={async () => {
-          sendNotification(80000);
-          setlastFood(Date.now());
-          try {
-            const nowDateString = String(Date.now());
-            await AsyncStorage.setItem('@lastFoodTimestamp', nowDateString);
-          } catch (e) {
-          }
-          setHunger(0);
-        }}
-      >
-        <Text style={{height: 20}}>Give food</Text>
-      </TouchableOpacity>
+      <View style={{ height: 40, paddingTop: 120 }}>
+        <TouchableOpacity
+          style={{ height: 100 }}
+          onPress={giveFood}
+        >
+          <Text style={{ height: 20 }}>Hunger: {100 - hunger}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ height: 100 }}
+          onPress={async () => {
+            sendNotification(80000);
+            setlastFood(Date.now());
+            try {
+              const nowDateString = String(Date.now());
+              await AsyncStorage.setItem('@lastFoodTimestamp', nowDateString);
+            } catch (e) {
+            }
+            setHunger(0);
+          }}
+        >
+          <Text style={{ height: 20 }}>Give food</Text>
+        </TouchableOpacity>
       </View>
       {isDead ?
-      <TouchableOpacity
-        style={{height: 80, width: 80, position: "absolute", top: screen.height/2 + 50, left: screen.width/2 -40, textAlign: 'center'}}
-        onPress={async () => {
-          sendNotification(80000);
-          setIsDead(false);
-          setlastFood(Date.now());
-          setHunger(0);
-      }}
-      >
-        <Text
-        style={{textAlign: 'center', fontSize: 25}}
+        <TouchableOpacity
+          style={{ height: 80, width: 80, position: "absolute", top: screen.height / 2 + 50, left: screen.width / 2 - 40, textAlign: 'center' }}
+          onPress={respawn}
         >
-          PET IS DEAD
+          <Text
+            style={{ textAlign: 'center', fontSize: 25 }}
+          >
+            PET IS DEAD
         </Text>
-      </TouchableOpacity>
-      :
-      <Image
-        style={{height: 80, width: 80, position: "absolute", top: screen.height/2 + 50, left: screen.width/2 -40}}
-        source={require('./assets/penguin.png')}
-      />
+        </TouchableOpacity>
+        :
+        <Image
+          style={{ height: 80, width: 80, position: "absolute", top: screen.height / 2 + 50, left: screen.width / 2 - 40 }}
+          source={require('./assets/penguin.png')}
+        />
       }
     </View>
   );
